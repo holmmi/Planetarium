a22758de70174498f748e591e5c4f257f3eb7974
Lauri Kettunen <laurim.kettunen@gmail.com>
2021-12-01 09:43:37 +0200

    favoriteslistview and favoritesitemview working, swpite to delete favorite from list

---
+++ PlanetariumApplication/Views/Favorites/FavoritesItemView.swift
@@ -0,0 +1,52 @@
+//
+//  FavoritesItemView.swift
+//  PlanetariumApplication
+//
+//  Created by Lauri Kettunen on 22.11.2021.
+//
+
+import SwiftUI
+
+struct FavoritesItemView: View {
+    
+    var favorite: Favorite
+    
+    @StateObject var favoritesItemViewModel: FavoritesItemViewModel = FavoritesItemViewModel()
+    
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                AsyncImage(url: URL(string: !favorite.video ? favorite.url! : favorite.thumbnailUrl!)) { image in
+                    image.resizable()
+                } placeholder: {
+                    ProgressView()
+                }.frame(height: 400)
+                Text(favorite.title!)
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                
+                if favorite.video {
+                    Link("Video", destination: URL(string: favorite.url!)!)
+                }
+                
+                Text(favorite.explanation!)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                Spacer()
+            }.padding()
+        }
+
+    }
+    
+
+    
+    
+    struct FavoritesItemView_Previews: PreviewProvider {
+        static var previews: some View {
+            FavoritesItemView(favorite: Favorite())
+        }
+    }
+    
+}
--- PlanetariumApplication/Views/Favorites/FavoritesListView.swift
+++ PlanetariumApplication/Views/Favorites/FavoritesListView.swift
@@ -2,19 +2,53 @@
 //  FavoritesListView.swift
 //  PlanetariumApplication
 //
-//  Created by Lauri Kettunen on 1.12.2021.
+//  Created by Lauri Kettunen on 22.11.2021.
 //
 
 import SwiftUI
 
 struct FavoritesListView: View {
+    @EnvironmentObject var favoritesListViewModel: FavoritesListViewModel
+        
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        NavigationView {
+            List {
+                ForEach(favoritesListViewModel.favorites) { favorite in
+                    NavigationLink(destination: FavoritesItemView(favorite: favorite), label:{
+                        HStack {
+                            AsyncImage(url: URL(string: !favorite.video ? favorite.url! : favorite.thumbnailUrl!)) { image in
+                                image.resizable()
+                            } placeholder: {
+                                ProgressView()
+                            }.frame(width: 120, height: 120)
+                            VStack(alignment: .leading) {
+                                Text(favorite.title!)
+                                    .font(.title2)
+                                Text(favorite.date!)
+                                    .padding(.top)
+                                Spacer()
+                            }
+                            Spacer()
+                        }
+                        
+                    })
 
+                }.onDelete(perform: favoritesListViewModel.deleteFavorite)
+            }
+            .listStyle(GroupedListStyle())
+            .navigationTitle("Favorites")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear{
+                favoritesListViewModel.getFavorites()
+            }
+        }
+        
+}
 struct FavoritesListView_Previews: PreviewProvider {
+    
+    
     static var previews: some View {
         FavoritesListView()
     }
+}
 }
